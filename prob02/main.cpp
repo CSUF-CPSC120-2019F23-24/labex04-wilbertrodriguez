5757364deaaf5655e293232af47243cb5e3c1e22
wilbertrodriguez <wilbertrodriguez@cs104u-6f.acad.fullerton.edu>
2019-09-14 13:57:56 -0700

    fixed bugs

--- prob02/main.cpp
+++ prob02/main.cpp
@@ -6,19 +6,19 @@ int main()
 {
   const int NUM_INTS = 3; // number of values to be averaged
 
-  int num1, num2, num3;
+  double num1, num2, num3;
   double average; // average of the numbers
 
     // get the numbers
     std::cout << "Enter three integers, each separated by one or more spaces: ";
-    cin >> num1 >> num2 >> num3;
+    std::cin >> num1 >> num2 >> num3;
 
-    calculate the average of the numbers
-       average = num1 + num2 + num3 / NUM_INTS;
+    //calculate the average of the numbers
+       average = (num1 + num2 + num3) / NUM_INTS;
 
   // display the average, including the decimal portion
   std::cout << "The average of " << num1 << ", " << num2 << ", and " << num3
-    << " is: " << average << Endl;
+    << " is: " << average << std::endl;
 
   return 0;
 }
